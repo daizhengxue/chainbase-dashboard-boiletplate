4351b3f55e777f42d2484bd4103901f4316c026e
daizhengxue <daizhengxue828@gmail.com>
2023-10-24 17:01:32 +0800

    add

--- src/app/dashboard/utils/tokenProfitQuery.ts
+++ src/app/dashboard/utils/tokenProfitQuery.ts
@@ -1,6 +1,6 @@
 import { TaskValue } from "@/components/QueryTaskProvider/context";
 
-export const getQueryString = () => `
+export const getQueryString = (address: string) => `
 WITH
     prices AS (
         SELECT
@@ -9,7 +9,7 @@ WITH
         FROM
             prices.token_prices
         WHERE
-            contract_address = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+            contract_address = '${address}'
             AND timestamp >= '2022-09-01'
     ),
     start_price AS (
@@ -42,7 +42,7 @@ WITH
         FROM
             ethereum.token_transfers
         WHERE
-            contract_address = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+            contract_address = '${address}'
             AND block_timestamp > '2022-09-01'
     ),
     buys AS (
@@ -92,20 +92,21 @@ ORDER BY
 `;
 
 export const tokenProfitQuery: TaskValue = {
-  id: 'token-profit-query',
-  task: async () => {
-    const res = await fetch('/api/chainbase/dw/query', {
-      method: 'POST',
-      body: JSON.stringify({
-        query: getQueryString(),
-      }),
-      headers: {
-        "Content-Type": 'application/json'
-      }
-    });
-    const result = await res.json();
-    return result?.data?.result
-  },
-  data: [],
-  status: 'idle'
-}
+    id: 'token-profit-query',
+    task: async (params: {address: string}) => {
+      const res = await fetch('/api/chainbase/dw/query', {
+        method: 'POST',
+        body: JSON.stringify({
+          query: getQueryString(params.address),
+        }),
+        headers: {
+          "Content-Type": 'application/json'
+        }
+      });
+      const result = await res.json();
+      return result?.data?.result
+    },
+    data: [],
+    status: 'idle'
+  }
+  
